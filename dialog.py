9058bc71d81c787f7c900d1d0a9cc87f1cbe54e0
John Paul <joaop.gomes@gmail.com>
2014-10-29 17:53:44 -0200

    atualizações na conversação

--- dialog.py
+++ dialog.py
@@ -1,9 +1,34 @@
 import pygame
 from global_data import *
+from pygame.locals import K_RETURN
 
 DIALOGUES = {}
 
-def dialog(width, height, npc, text):
+def dialog(player_position, other_position, conversation):
+    font_file = u'/usr/share/fonts/truetype/ttf-khmeros-core/KhmerOS.ttf'
+    font_render = pygame.font.Font(font_file, 14)
+    dialog_count = 0
+    while True:
+        for ev in pygame.event.get():
+            if ev.type == pygame.KEYDOWN:
+                if pygame.key.get_pressed()[K_RETURN]:
+                    if dialog_count >= len(DIALOGUES[conversation]):
+                        break
+                    speech = DIALOGUES[conversation][dialog_count]
+                    speech_ = font_render.render(speech[0]+': '+speech[1], True, pygame.Color(0, 0, 0))
+                    width = speech_.get_width()+30
+                    height = speech_.get_height()+30
+                    border_top = screen_height-height-30
+                    border_left = (screen_width-width)/2
+                    print (border_top, border_left, width, height)
+                    rect = pygame.Rect(border_top, border_left, width, height)
+                    pygame.draw.rect(screen, pygame.Color(10,10,211), rect, 2)
+                    dialog_count+=1
+                    pygame.time.Clock().tick(30)
+                    
+        
+    
+    """
     #if npc.has_dialog or npg.current_dialog >= len(npc.dialogues):
     #    return None
     
@@ -26,9 +51,10 @@ def dialog(width, height, npc, text):
         
     for s in text_surfs:
         screen.blit(s, (100, 100))
-        
+    """ 
 def load_dialog(file_name):
     dialog = open(u'data/conversation/%s.conv'%(file_name), 'r')
+    DIALOGUES[file_name] = []
     for speech in dialog:
-        print speech.split(';')
-    pass
+        speech_data = speech.split(';')
+        DIALOGUES[file_name].append(speech_data)
--- game.py
+++ game.py
@@ -35,6 +35,63 @@ class Game:
             return 0
         return y
     
+    def key_return(self):
+        x_, y_ = 0, 0
+        if self.last_input[K_LEFT]:
+            x_ = self.check_map_width((self.map.camera.x*(-1)+self.player.x)/32) 
+            y_ = self.check_map_height((self.map.camera.y*(-1)+self.player.y+42)/32)                
+        elif self.last_input[K_RIGHT]:
+            x_ = self.check_map_width((self.map.camera.x*(-1)+self.player.x+24)/32)
+            y_ = self.check_map_height((self.map.camera.y*(-1)+self.player.y+42)/32)
+        elif self.last_input[K_UP]:
+            x_ = self.check_map_width((self.map.camera.x*(-1) + self.player.x+16)/32)
+            y_ = self.check_map_height((self.map.camera.y*(-1) + self.player.y+16*(1))/32)
+        elif self.last_input[K_DOWN]:
+            x_ = self.check_map_width((self.map.camera.x*(-1) + self.player.x+16)/32)
+            y_ = self.check_map_height((self.map.camera.y*(-1) + self.player.y+48)/32)
+        if self.map.map_tiles[x_][y_].object is not None and self.map.map_tiles[x_][y_].object.type == 'NPC':
+            if self.map.map_tiles[x_][y_].object.has_dialog:
+                file_name = u'%s_%s'%(self.player.name, self.map.map_tiles[x_][y_].object.name)
+                if file_name.upper() not in dialog.DIALOGUES:
+                    dialog.load_dialog(file_name.upper())
+                dialog.dialog((self.player.get_position()), self.map.map_tiles[x_][y_].object.get_position(), file_name)
+    
+    def move_left(self, x_, y_):
+        if self.map.map_tiles[x_][y_].can_walk:
+            if not self.map.move(self.walk_speed, 0):
+                if not (self.player.x + self.walk_speed*(-1)) < 0:
+                    self.player.move(self.walk_speed*(-1), 0)
+        self.player.change_sprite(self.walk_speed*(-1), 0)
+    
+    def move_right(self, x_, y_):
+        if self.map.map_tiles[x_][y_].can_walk:
+            if not self.map.move(self.walk_speed*(-1), 0):
+                if not (self.player.x + self.walk_speed) > global_data.screen_width - (self.player.image.get_width()/4):
+                    self.player.move(self.walk_speed, 0)
+        self.player.change_sprite(self.walk_speed, 0)
+
+    def move_up(self, x_, y_):
+        if self.map.map_tiles[x_][y_].can_walk:
+            if not self.map.move(0, self.walk_speed):
+                if not (self.player.y + self.walk_speed*(-1)) < 0:
+                    self.player.move(0, self.walk_speed*(-1))
+        elif self.map.map_tiles[x_][y_].object is not None:# and self.map.map_tiles[x_][y_].object.type == 'PORTAL':
+            self.object_map_collider(self.map.map_tiles[x_][y_].object, x_, y_)
+        self.player.change_sprite(0, self.walk_speed*(-1))
+
+    def move_down(self, x_, y_):
+        if self.map.map_tiles[x_][y_].can_walk:
+            if not self.map.move(0, self.walk_speed*(-1)):
+                if not (self.player.y + self.walk_speed) > global_data.screen_height - (self.player.image.get_height()/4):
+                    self.player.move(0, self.walk_speed)
+        self.player.change_sprite(0, self.walk_speed)
+
+    def object_map_collider(self, object, X_, Y_):
+        if object.type == 'PORTAL':
+            if not self.map.map_tiles[x_][y_].object.name in self.maps:
+                self.maps[self.map.map_tiles[x_][y_].object.name] = Map(self.map.map_tiles[x_][y_].object.name) 
+            self.map = self.maps[self.map.map_tiles[x_][y_].object.name]
+
     """ Recebe o input do jogador e realiza a ação de acordo com a tecla pressionada """
     def player_input(self, key_pressed):
         """
@@ -42,79 +99,32 @@ class Game:
         camera em relação ao mapa e do personagem e do personagem em relação a camera
         """
         #x_, y_ = (self.map.camera.x*(-1)+self.player.x)/32, (self.map.camera.y*(-1)+self.player.y)/32
-  
+         
         if key_pressed[K_RETURN]:
-            x_, y_ = 0, 0
-            if self.last_input[K_LEFT]:
-                x_ = self.check_map_width((self.map.camera.x*(-1)+self.player.x)/32) 
-                y_ = self.check_map_height((self.map.camera.y*(-1)+self.player.y+42)/32)                
-            elif self.last_input[K_RIGHT]:
-                x_ = self.check_map_width((self.map.camera.x*(-1)+self.player.x+24)/32)
-                y_ = self.check_map_height((self.map.camera.y*(-1)+self.player.y+42)/32)
-            elif self.last_input[K_UP]:
-                x_ = self.check_map_width((self.map.camera.x*(-1) + self.player.x+16)/32)
-                y_ = self.check_map_height((self.map.camera.y*(-1) + self.player.y+16*(1))/32)
-            elif self.last_input[K_DOWN]:
-                x_ = self.check_map_width((self.map.camera.x*(-1) + self.player.x+16)/32)
-                y_ = self.check_map_height((self.map.camera.y*(-1) + self.player.y+48)/32)
-            
-            if self.map.map_tiles[x_][y_].object is not None and self.map.map_tiles[x_][y_].object.type == 'NPC':
-                print u'load_dialog'
-                if self.map.map_tiles[x_][y_].object.has_dialog:
-                    file_name = u'%s_%s'%(self.player.name, self.map.map_tiles[x_][y_].object.name)
-                    if file_name.upper() not in dialog.DIALOGUES:
-                        dialog.load_dialog(file_name.upper())
-                    pass
-        if key_pressed[K_LEFT]:
+            self.key_return()
+        
+        elif key_pressed[K_LEFT]:
             x_ = self.check_map_width((self.map.camera.x*(-1)+self.player.x)/32) 
             y_ = self.check_map_height((self.map.camera.y*(-1)+self.player.y+42)/32)
-
-            if self.map.map_tiles[x_][y_].can_walk:
-                if not self.map.move(self.walk_speed, 0):
-                    if not (self.player.x + self.walk_speed*(-1)) < 0:
-                        self.player.move(self.walk_speed*(-1), 0)
-            elif self.map.map_tiles[x_][y_].object is not None and self.map.map_tiles[x_][y_].object.type == 'PORTAL':
-                pass
-            self.player.change_sprite(self.walk_speed*(-1), 0)
+            self.move_left(x_, y_)
 
         elif key_pressed[K_RIGHT]:
             x_ = self.check_map_width((self.map.camera.x*(-1)+self.player.x+24)/32)
             y_ = self.check_map_height((self.map.camera.y*(-1)+self.player.y+42)/32)
-
-            if self.map.map_tiles[x_][y_].can_walk:
-                if not self.map.move(self.walk_speed*(-1), 0):
-                    if not (self.player.x + self.walk_speed) > global_data.screen_width - (self.player.image.get_width()/4):
-                        self.player.move(self.walk_speed, 0)
-            elif self.map.map_tiles[x_][y_].object is not None and self.map.map_tiles[x_][y_].object.type == 'PORTAL':
-                pass
-            self.player.change_sprite(self.walk_speed, 0)
+            self.move_right(x_, y_)
 
         elif key_pressed[K_UP]:
             x_ = self.check_map_width((self.map.camera.x*(-1) + self.player.x+16)/32)
             y_ = self.check_map_height((self.map.camera.y*(-1) + self.player.y+16*(1))/32)
-
-            if self.map.map_tiles[x_][y_].can_walk:
-                if not self.map.move(0, self.walk_speed):
-                    if not (self.player.y + self.walk_speed*(-1)) < 0:
-                        self.player.move(0, self.walk_speed*(-1))
-            elif self.map.map_tiles[x_][y_].object is not None and self.map.map_tiles[x_][y_].object.type == 'PORTAL':
-                if not self.map.map_tiles[x_][y_].object.name in self.maps:
-                    self.maps[self.map.map_tiles[x_][y_].object.name] = Map(self.map.map_tiles[x_][y_].object.name) 
-                self.map = self.maps[self.map.map_tiles[x_][y_].object.name]                
-            self.player.change_sprite(0, self.walk_speed*(-1))
+            self.move_up(x_, y_)
 
         elif key_pressed[K_DOWN]:
             x_ = self.check_map_width((self.map.camera.x*(-1) + self.player.x+16)/32)
             y_ = self.check_map_height((self.map.camera.y*(-1) + self.player.y+48)/32)
-
-            if self.map.map_tiles[x_][y_].can_walk:
-                if not self.map.move(0, self.walk_speed*(-1)):
-                    if not (self.player.y + self.walk_speed) > global_data.screen_height - (self.player.image.get_height()/4):
-                        self.player.move(0, self.walk_speed)
-            elif self.map.map_tiles[x_][y_].object is not None and self.map.map_tiles[x_][y_].object.type == 'PORTAL':
-                pass
-            self.player.change_sprite(0, self.walk_speed)
+            self.move_down(x_, y_)
+            
         self.last_input = key_pressed
+
     def main_loop(self):
         while True:
             for ev in event.get():
@@ -125,7 +135,6 @@ class Game:
                     self.player_input(pygame.key.get_pressed())
                 self.map.draw_map()
                 self.player.draw()
-                dialog.dialog(100, 100, None, [u'Testando a aplicação'])
                 pygame.display.update()
             self.clock.tick(30)
 
