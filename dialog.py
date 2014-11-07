ae5ff3a8a510fac0e9e37984e877d32d25420f0d
John Paul <joaop.gomes123@gmail.com>
2014-11-07 17:08:02 -0200

    trash removed

--- dialog.py
+++ dialog.py
@@ -9,6 +9,7 @@ def dialog(player, map, other_position, conversation):
     font_render = pygame.font.Font(font_file, 14)
     dialog_count = 0
     while True:
+        """open box dialog, create dialog box, close box pass """
         for ev in pygame.event.get():
             if ev.type == pygame.KEYDOWN:
                 if pygame.key.get_pressed()[K_RETURN]:
@@ -32,33 +33,7 @@ def dialog(player, map, other_position, conversation):
                     dialog_count+=1
                     pygame.display.update()
                     pygame.time.Clock().tick(30)
-                    
-        
-    
-    """
-    #if npc.has_dialog or npg.current_dialog >= len(npc.dialogues):
-    #    return None
-    
-    text_surfs = []
-    
-    rect = pygame.Rect(85, 92, 165, 44)
-    #@screen.blit(pygame.image.load('/home/john/workspace/rpg-v2/images/grass.gif').convert_alpha(), (0, 0), rect)
-    pygame.draw.rect(screen, pygame.Color(10,10,211), rect, 2)
-    rect = pygame.Rect(87, 94, 161, 40)
-    #pygame.draw.rect(screen, pygame.Color(180,180,180), rect)
-    s = pygame.Surface((161, 40))
-    s.set_alpha(128)
-    s.fill((180, 180, 180))
-    screen.blit(s, (87, 94))
-    
-    for text_line in text:
-        t = pygame.font.Font(u'/usr/share/fonts/truetype/ttf-khmeros-core/KhmerOS.ttf', 14).render(text_line, True, pygame.Color(0, 0, 0)) 
-        text_surfs.append(t)
-        #print t.get_width()
-        
-    for s in text_surfs:
-        screen.blit(s, (100, 100))
-    """ 
+
 def load_dialog(file_name):
     dialog = open(u'data/conversation/%s.conv'%(file_name), 'r')
     DIALOGUES[file_name] = []
